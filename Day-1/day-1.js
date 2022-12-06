47042564ea82f265bca8abb83815f44f11e93df2
Olli-Heikki Inkinen <inkinenolliheikki@gmail.com>
2022-12-06 13:37:20 +0200

    Day 1 solutions

---
+++ Day-1/day-1.js
@@ -0,0 +1,59 @@
+/*
+Part one:
+
+The task for the part one of day 1 is to find the elf that is
+carrying the most calories. The data is listed in the "input.txt".
+*/
+
+const fs = require('fs');
+
+let data = fs.readFileSync("input.txt").toString().split("\n\n")
+
+let calories = [];
+
+data.forEach(elf => {
+    calories.push(elf.split("\n"));
+});
+
+let most_calories = 0;
+
+calories.forEach(elf => {
+    let sum = elf.reduce((a, b) => parseInt(a) + parseInt(b), 0);
+
+    if (sum > most_calories) most_calories = sum;
+})
+
+console.log(most_calories);
+
+/*
+Part two:
+
+The task for the part two of day 1 is to find the three elfs that
+are carrying the most calories and the total amount of calories
+that they are carrying.
+*/
+
+let top_three_calories = [0, 0, 0];
+
+calories.forEach(elf => {
+    let sum = elf.reduce((a, b) => parseInt(a) + parseInt(b), 0);
+
+    switch (true) {
+        case (top_three_calories[0] < sum):
+            top_three_calories.unshift(sum);
+            top_three_calories.pop();
+            break;
+
+        case (top_three_calories[1] < sum):
+            top_three_calories.splice(1, 0, sum);
+            top_three_calories.pop();
+            break;
+
+        case (top_three_calories[2] < sum):
+            top_three_calories.splice(2, 0, sum);
+            top_three_calories.pop();
+            break;
+    }
+})
+
+console.log(top_three_calories.reduce((a, b) => a + b, 0));
